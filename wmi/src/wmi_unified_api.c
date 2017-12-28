19ba268a9e02063ee78b7a324be02d7ad48f7f40
Jean-Baptiste Theou <jb@essential.com>
2017-12-28 10:04:32 -0800

    fast-forward to LA.UM.6.4.r1-05700-8x98.0
    Change-Id: Ie046cb40c8178e21fd20696b48420500f9e96982

--- wmi/src/wmi_unified_api.c
+++ wmi/src/wmi_unified_api.c
@@ -2946,29 +2946,6 @@ QDF_STATUS wmi_unified_update_tdls_peer_state_cmd(void *wmi_hdl,
 }
 
 /**
- * wmi_unified_process_fw_mem_dump_cmd() - Function to request fw memory dump from
- * firmware
- * @wmi_handle:         Pointer to wmi handle
- * @mem_dump_req:       Pointer for mem_dump_req
- *
- * This function sends memory dump request to firmware
- *
- * Return: QDF_STATUS_SUCCESS on success and QDF_STATUS_E_FAILURE for failure
- *
- */
-QDF_STATUS wmi_unified_process_fw_mem_dump_cmd(void *wmi_hdl,
-					struct fw_dump_req_param *mem_dump_req)
-{
-	wmi_unified_t wmi_handle = (wmi_unified_t) wmi_hdl;
-
-	if (wmi_handle->ops->send_process_fw_mem_dump_cmd)
-		return wmi_handle->ops->send_process_fw_mem_dump_cmd(wmi_handle,
-			    mem_dump_req);
-
-	return QDF_STATUS_E_FAILURE;
-}
-
-/**
  * wmi_unified_process_set_ie_info_cmd() - Function to send IE info to firmware
  * @wmi_handle:    Pointer to WMi handle
  * @ie_data:       Pointer for ie data
@@ -6380,6 +6357,19 @@ QDF_STATUS wmi_unified_send_dbs_scan_sel_params_cmd(void *wmi_hdl,
 		return wmi_handle->ops->
 			send_dbs_scan_sel_params_cmd(wmi_handle,
 				  dbs_scan_params);
+
+	return QDF_STATUS_E_FAILURE;
+}
+
+QDF_STATUS
+wmi_unified_send_action_oui_cmd(void *wmi_hdl,
+				struct wmi_action_oui *action_oui)
+{
+	wmi_unified_t wmi_handle = (wmi_unified_t)wmi_hdl;
+
+	if (wmi_handle->ops->send_action_oui_cmd)
+		return wmi_handle->ops->send_action_oui_cmd(wmi_handle,
+							    action_oui);
 
 	return QDF_STATUS_E_FAILURE;
 }
